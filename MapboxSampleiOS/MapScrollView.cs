966a3f1fc2792f7ebc39ce937956ad14f46eee6a
s <s>
2017-03-22 11:56:35 -0400

    n

--- MapboxSampleiOS/MapScrollView.cs
+++ MapboxSampleiOS/MapScrollView.cs
@@ -90,11 +90,11 @@ namespace StateMaps
         {
 			SVGKFastTileView map = new SVGKFastTileView(new CGRect(0,0,256,256), xOffset);
             CGRect bounds = this.ConvertRectToView(this.Bounds, map);
-            UILabel label1 = new UILabel(new CGRect(bounds.GetMinX(), 300, 200, 80));
-            UILabel label2 = new UILabel(new CGRect(bounds.GetMaxX(), 300, 200, 80));            
-            label1.Text = bounds.GetMinX().ToString();
-            label2.Text = bounds.GetMaxX().ToString();
-			this.AddSubviews(map,label1,label2);
+            UILabel label1 = new UILabel(new CGRect(0, 0, 200, 80));
+            UILabel label2 = new UILabel(new CGRect(0, 0, 200, 80));            
+            label1.Text = "que";
+            label2.Text = "lo que";
+			this.AddSubview(label1);
 			return map;
         }
 
